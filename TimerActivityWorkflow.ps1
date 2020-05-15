52f47b4e41d070359967c881dea8aaa19e9bd76f
BrettMoff <brett@systematec.com.au>
2020-05-15 11:09:16 +0930

    Initial files
    Creation of inital files

---
+++ TimerActivityWorkflow.ps1
@@ -0,0 +1,15 @@
+﻿$enumActive = (Get-SCSMEnumeration ActivityStatusEnum.Active$).Id
+$enumOnHold = (Get-SCSMEnumeration ActivityStatusEnum.OnHold$).Id
+$enumCompleted = (Get-SCSMEnumeration ActivityStatusEnum.Completed$).Id
+
+$ActiveTimers = get-scsmobject -class(get-scsmclass -name timer_Activity) -filter "Status -eq $enumActive -or Status -eq $enumOnHold"
+
+Try {
+    ForEach ($Activity in $ActiveTimers) {
+        If ((Get-Date) -gt $Activity.ScheduledEndTime) {
+            $ActivityHash = @{"Status" = $enumCompleted}
+            Set-SCSMObject -SMObject $Activity -PropertyHashTable $ActivityHash}
+    }
+}
+Catch{
+    out-file "C:\temp\timererror.log" -InputObject $_.exception.message -append}
