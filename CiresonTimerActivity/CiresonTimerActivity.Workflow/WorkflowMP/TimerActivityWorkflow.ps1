6535667cd5a61ab4162603d30fdd072fc26563c4
Brett Moffett <brett@systematec.com.au>
2020-06-15 20:40:18 +0930

    first draft....   Hopefully

--- CiresonTimerActivity/CiresonTimerActivity.Workflow/WorkflowMP/TimerActivityWorkflow.ps1
+++ CiresonTimerActivity/CiresonTimerActivity.Workflow/WorkflowMP/TimerActivityWorkflow.ps1
@@ -1,15 +1,29 @@
 ﻿$enumActive = (Get-SCSMEnumeration ActivityStatusEnum.Active$).Id
 $enumOnHold = (Get-SCSMEnumeration ActivityStatusEnum.OnHold$).Id
 $enumCompleted = (Get-SCSMEnumeration ActivityStatusEnum.Completed$).Id
+$LogPath = (get-scsmsetting -displayname "Cireson Timer Activity Settings").LogPath
+$LogEnable = (get-scsmsetting -displayname "Cireson Timer Activity Settings").Logenable
 
-$ActiveTimers = get-scsmobject -class(get-scsmclass -name timer_Activity) -filter "Status -eq $enumActive -or Status -eq $enumOnHold"
-
+$ActiveTimers = get-scsmobject -class(get-scsmclass -name Cireson.Timer.Activity$) -filter "Status -eq $enumActive -or Status -eq $enumOnHold"
+LogThis ("Number of active Timer Activities found: " + $ActiveTimers.count) -LogType "Start"
 Try {
     ForEach ($Activity in $ActiveTimers) {
         If ((Get-Date) -gt $Activity.ScheduledEndTime) {
             $ActivityHash = @{"Status" = $enumCompleted}
             Set-SCSMObject -SMObject $Activity -PropertyHashTable $ActivityHash}
+            LogThis ($Activity.id + " set to completed.") -LogType "Info"
     }
 }
 Catch{
-    out-file "C:\temp\timererror.log" -InputObject $_.exception.message -append}
+    Logthis $_.exception.message -LogType "Error"
+    }
+
+Function LogThis {
+    #This function is to allow log files to be written as needed
+    param($LogText, $LogType)
+
+    if ($LogEnable -eq 1) {
+        $LogDate = get-date
+        "$LogDate - $LogType - $logtext" | Out-File -filepath "$LogPath\TimerActivity.log" -append -encoding ascii
+    }
+}
